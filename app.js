e00384ef616895ebb7c22e0032b1af3f88816d2f
zarathmgd <zarathmougamadou@gmail.com>
2023-07-13 15:53:55 +0200

    fix speed up in double click issue

--- app.js
+++ app.js
@@ -7,72 +7,71 @@ let seconds = 0;
 let timerText = document.getElementById("timer-text");
 let timer;
 
-let timeRefresh = function() {
-    timer = setInterval(() => {
-        seconds++;
+let timeRefresh = function () {
+  timer = setInterval(() => {
+    seconds++;
 
-        
-        if (seconds < 10) {
-            seconds = "0" + seconds;
-        }
-        if (minutes < 10) {
-            minutes = "0" + minutes;
-        }
-        if (hours < 10) {
-            hours = "0" + hours;
-        }
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
 
-        if (seconds > 60) {
-            seconds = 0;
-            minutes++;
-        }
+    if (seconds > 60) {
+      seconds = 0;
+      minutes++;
+    } else if (minutes > 60) {
+      minutes = 0;
+      hours++;
+    }
 
-        else if (minutes > 60) {
-            minutes = 0;
-            hours++;
-        }
-
-        timerText.innerHTML = hours.toString().slice(-2) + " : " + minutes.toString().slice(-2) + " : " + seconds;
-}, 1000)};
+    timerText.innerHTML = hours.toString().slice(-2) + " : " + minutes.toString().slice(-2) + " : " + seconds;
+  }, 1000);
+};
 
 buttonOne.addEventListener("click", () => {
-    timeRefresh();
+  clearInterval(timer);
+  timeRefresh();
 });
 
 buttonTwo.addEventListener("click", () => {
-    clearInterval(timer);
-})
+  clearInterval(timer);
+});
 
 buttonThree.addEventListener("click", () => {
-    clearInterval(timer);
-    hours = 0;
-    minutes = 0;
-    seconds = 0;
-    timerText.innerHTML = "0" + hours.toString().slice(-2) + " : " + "0" + minutes.toString().slice(-2) + " : " + "0" + seconds;
-})
+  clearInterval(timer);
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+  timerText.innerHTML = "0" + hours.toString().slice(-2) + " : " + "0" + minutes.toString().slice(-2) + " : " + "0" + seconds;
+});
 
-function backgroundAnimation () {
-    const count = 100;
-    const body = document.querySelector("body");
-    var i = 0;
+function backgroundAnimation() {
+  const count = 100;
+  const body = document.querySelector("body");
+  var i = 0;
 
-    while (i < count) {
-        const ball = document.createElement("i");
-        const x = Math.floor(Math.random() * window.innerWidth);
-        const y = Math.floor(Math.random() * window.innerHeight * 2);
+  while (i < count) {
+    const ball = document.createElement("i");
+    const x = Math.floor(Math.random() * window.innerWidth);
+    const y = Math.floor(Math.random() * window.innerHeight * 2);
 
-        const size = Math.random() * 4;
-        ball.style.left = x + "px";
-        ball.style.top = y + "px";
-        ball.style.width = 1 + size + "px";
-        ball.style.height = 1 + size + "px";
-        ball.className = "ball";
-        ball.style.animationDelay = Math.random() * -20 + "s";
-        ball.style.animationDuration = 20 + Math.random() + "s";
+    const size = Math.random() * 4;
+    ball.style.left = x + "px";
+    ball.style.top = y + "px";
+    ball.style.width = 1 + size + "px";
+    ball.style.height = 1 + size + "px";
+    ball.className = "ball";
+    ball.style.animationDelay = Math.random() * -20 + "s";
+    ball.style.animationDuration = 20 + Math.random() + "s";
 
-        body.appendChild(ball);
-        i++;
-    };
-};
+    body.appendChild(ball);
+    i++;
+  }
+}
 
 backgroundAnimation();
